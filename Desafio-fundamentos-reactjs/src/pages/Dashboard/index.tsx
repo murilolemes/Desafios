8d264e1d4038f8ca57f26f3a77a40665a7657a92
murilolemes <murilo.lemes.mhl@gmail.com>
2020-08-24 10:13:07 -0300

    melhorias no desafio fundamentos reactjs - formatValue -

--- Desafio-fundamentos-reactjs/src/pages/Dashboard/index.tsx
+++ Desafio-fundamentos-reactjs/src/pages/Dashboard/index.tsx
@@ -71,30 +71,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">
-              R$
-              {balance.income}
-            </h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">
-              R$
-              {balance.outcome}
-            </h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">
-              R$
-              {balance.total}
-            </h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
